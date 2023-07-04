b6e08e7115d4e762302bad16acd3545a7b8c5ed4
Масанов Александр Николаевич <masanov-1996@yandex.ru>
2023-07-04 16:16:57 +0700

    Add files via upload

---
+++ Calc.java
@@ -0,0 +1,35 @@
+
+public class Calc {
+
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 3;
+        int result = 1;
+        char sign = '+';
+
+        if (sign == '+') {
+            result = a + b;
+
+        } else if (sign == '-') {
+            result = a - b;
+
+        } else if (sign == '*') {
+            result = a * b;
+
+        } else if (sign == '/') {
+            result = a / b;
+
+        } else if (sign == '^') {
+            result = 1;
+            for (int i = 0; i < b; i++) {
+                result = result * a;
+            }
+
+        } else if (sign == '%') {
+            result = a % b;
+        }
+
+        System.out.println(a + " " + sign + " " + b + " = " + result);
+    }
+
+}
---
+++ Number.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class Number {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int pcNumber = random.nextInt(100) + 1;
+        int gamerNumber = 50;
+        while (gamerNumber != pcNumber) {
+            if (gamerNumber < pcNumber) {
+                System.out.println(gamerNumber + " число меньше того, что загадал компьютер");
+                gamerNumber++;
+            } else if (gamerNumber > pcNumber) {
+                System.out.println(gamerNumber + " число больше того, что загадал компьютер");
+                gamerNumber--;
+            }
+        }
+        System.out.println("Вы победили! " + pcNumber);
+    }
+}
